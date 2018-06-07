dc64cbe85b88d9c4c06b9819c4faf67ceb7661fa
Lbh <544289495@qq.com>
2018-06-07 17:28:21 +0800

    Router之Switch 与Redirect

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,15 @@
+
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import '../styles/nav.css'
 const NavBar = () =>(
     <div>
         <div>
-            <NavLink exact to = "/">lbh</NavLink>
-            <NavLink to = "/bh1">lbh1</NavLink>
-            <NavLink to = "/bh2">lbh2</NavLink>
+            <NavLink exact to = "/">lbh</NavLink>|&nbsp;
+            <NavLink to = "/bh1" activeClassName = "activeNav">lbh1</NavLink>|&nbsp;
+            <NavLink to = "/bh2" activeClassName = "activeNav">lbh2</NavLink>|&nbsp;
+            <NavLink  from = "/redirect" to="/bh1" activeClassName = "activeNav">redirect</NavLink>|&nbsp;
+            <NavLink to = "/react" activeClassName = "activeNav">404</NavLink>&nbsp;
         </div>
     </div>
 )
--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Route,Switch,Redirect} from 'react-router-dom'
 import App from './components/Main';
 
 import lbh from './components/Lbh';
 import lbh1 from './components/Lbh.1';
 import lbh2 from './components/Lbh.2';
 import Nav from './components/nav';
+import Error from './components/404';
 
 // Render the main component into the dom
 ReactDOM.render(
     <Router>
         <div>
             <Nav/>
-            <Route exact path='/' component={lbh}/>
-            <Route path='/bh1' component={lbh1}/>
-            <Route path='/bh2' component={lbh2}/>
+            <Switch>
+                <Route exact path='/' component={lbh}/>
+                <Route path='/bh1' component={lbh1}/>
+                <Route path='/bh2' component={lbh2}/>
+                <Redirect from="/redirect" to="/bh1" />
+                <Route component={Error}/>
+            </Switch>
         </div>
     </Router>
 , document.getElementById('app'));
